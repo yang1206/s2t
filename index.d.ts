c7e3127338b91f04907e77a2bf14eda5316628b9
yangjunwei <yangjunwei@haocean.com>
2022-10-09 11:32:13 +0800

    release 0.0.3

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,23 @@ interface Custom {
     des: string;
 }
 declare type CustomStr = Array<Custom>;
-declare const Chinese: (str: string, type: 's2t' | 't2s', custom?: CustomStr) => string | undefined;
+declare const Chinese: {
+    /**
+     *
+     * @param {(str,custom) => string}
+     * @param {string} str 需要转换的字符
+     * @param {CustomStr} custom 自定义的字符
+     * @returns {string}
+     */
+    s2t: (str: string, custom?: CustomStr) => string;
+    /**
+     *
+     * @param {(str,custom) => string}
+     * @param {string} str 需要转换的字符
+     * @param {CustomStr} custom 自定义的字符
+     * @returns {string}
+     */
+    t2s: (str: string, custom?: CustomStr) => string;
+};
 
 export { Chinese, CustomStr };
