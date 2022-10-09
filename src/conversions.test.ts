96f67491692f4097e90ad1974734867455990385
yang1206 <yang15188280673@outlook.com>
2022-10-09 11:32:13 +0800

    release 0.0.3

--- src/conversions.test.ts
+++ src/conversions.test.ts
@@ -2,6 +2,7 @@ import { expect, it } from 'vitest'
 import { Chinese } from './conversions'
 
 it('Chinese', () => {
-  expect(Chinese('大哥大嫂过年好', 's2t'))
-  expect(Chinese('大哥大嫂過年好', 't2s'))
+  expect(Chinese.s2t('大哥大嫂过年好'))
+  expect(Chinese.t2s('大哥大嫂過年好'))
+  expect(Chinese.t2s('超級戰士 大戰  秋風之敦', [{ src: '超級戰士', des: '斯巴达战士' }, { src: '秋風之敦', des: '秋之柱' }]))
 })
--- src/conversions.ts
+++ src/conversions.ts
@@ -7,68 +7,86 @@ export type CustomStr = Array<Custom>
 /**
  * 简体字
  */
-const S = zhData.S;
+const S = zhData.S
 
 /**
  * 繁体字
  */
-const T = zhData.T;
+const T = zhData.T
 
-
-const tranStr = (str: string, toT: boolean, custom?: CustomStr): string => {
-  if (custom) {
-    for (let s = 0; s < custom.length; s++) {
-      if (str.includes(custom[s].src)) {
-        str = str.replaceAll(custom[s].src, custom[s].des)
-      }
-    }
-  }
-  let src;
-  let des;
-  let i: number;
-  let letter;
-  let result: string = '';
-  let index: number;
+const tranStr = (str: string, toT: boolean): string => {
+  let src
+  let des
+  let i: number
+  let letter
+  let result = ''
+  let index: number
   if (toT) {
-    src = S;
-    des = T;
-  } else {
-    src = T;
-    des = S;
+    src = S
+    des = T
+  }
+  else {
+    src = T
+    des = S
   }
   for (i = 0; i < str.length; i++) {
-    letter = str.charAt(i);
-    const code = str.charCodeAt(i);
+    letter = str.charAt(i)
+    const code = str.charCodeAt(i)
     // 根据字符的Unicode判断是否为汉字，以提高性能
-    const isChinese: boolean = (code > 0x3400 && code < 0x9FC3) || (code > 0xF900 && code < 0xFA6A);
+    const isChinese: boolean
+      = (code > 0x3400 && code < 0x9FC3) || (code > 0xF900 && code < 0xFA6A)
     if (!isChinese) {
-      result += letter;
-      continue;
+      result += letter
+      continue
     }
 
-    index = src.indexOf(letter);
+    index = src.indexOf(letter)
 
-    if (index !== -1) {
-      result += des.charAt(index);
-    } else {
-      result += letter;
-    }
+    if (index !== -1)
+      result += des.charAt(index)
+    else result += letter
   }
-  return result;
+  return result
 }
 
-
-
-export const Chinese = (str: string, type: 's2t' | 't2s', custom?: CustomStr) => {
-  if (type === 's2t') {
-    if (custom && custom.length !==0) {
-      return tranStr(str, true, custom)
+export const Chinese = {
+  /**
+   *
+   * @param {(str,custom) => string}
+   * @param {string} str 需要转换的字符
+   * @param {CustomStr} custom 自定义的字符
+   * @returns {string}
+   */
+  s2t: (str: string, custom?: CustomStr): string => {
+    if (custom) {
+      for (let s = 0; s < custom.length; s++) {
+        if (str.includes(custom[s].src))
+          str = str.replaceAll(custom[s].src, custom[s].des)
+      }
+      return tranStr(str, true)
     }
-    return tranStr(str, true)
-  } else if (type === 't2s') {
-    if (custom && custom.length !== 0) {
-      return tranStr(str, false, custom)
+    else {
+      return tranStr(str, true)
     }
-    return tranStr(str, false)
-  }
+  },
+  /**
+   *
+   * @param {(str,custom) => string}
+   * @param {string} str 需要转换的字符
+   * @param {CustomStr} custom 自定义的字符
+   * @returns {string}
+   */
+  t2s: (str: string, custom?: CustomStr): string => {
+    if (custom) {
+      for (let s = 0; s < custom.length; s++) {
+        if (str.includes(custom[s].src))
+          str = str.replaceAll(custom[s].src, custom[s].des)
+      }
+      return tranStr(str, false)
+    }
+    else {
+      return tranStr(str, false)
+    }
+  },
 }
+
